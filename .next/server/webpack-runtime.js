c6379eab684dc646a05c0d9f1ffcdd285eda86e0
fsd-niraj <develop.niraj@gmail.com>
2025-05-04 19:20:24 -0400

    blog pages layout

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e7c411f51da01afa")
+/******/ 		__webpack_require__.h = () => ("1dcd5352562dbe6b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/app/[blog]/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+
+    <div className="max-w-2xl m-auto p-8 font-[family-name:var(--font-geist-sans)] flex flex-col h-screen gap-4 cursor-default">
+      blog
+    </div>
+  )
+}
