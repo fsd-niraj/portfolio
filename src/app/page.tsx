dd3bb12b685213eb7c59d010cb1a886c4ec842cd
fsd-niraj <develop.niraj@gmail.com>
2025-05-04 20:51:40 -0400

    motion react - homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client";
 import { FlipWords } from "@/components/ui/Filpwords";
 import { homePageData } from "../data";
+import { motion, Variants } from "motion/react";
 
 export default function Home() {
 
   const Anchor = ({ href, title }: { href: string, title: string }) => {
     return (<>
-      <a href={href} target="_blank" rel="noopener noreferrer" className="text-gray-500 dark:text-gray-500 hover:underline hover:text-green-600 dark:hover:text-green-400 transition-colors duration-400">{title}</a>
+      <motion.a variants={LIST_ITEM_VARIANTS} href={href} target="_blank" rel="noopener noreferrer" className="text-gray-500 dark:text-gray-500 hover:underline hover:text-green-600 dark:hover:text-green-400 transition-colors duration-400">{title}</motion.a>
       &nbsp;
     </>
     )
@@ -15,41 +17,62 @@ export default function Home() {
     return <span className="text-black dark:text-white">{text}</span>
   }
 
+  const LIST_CONTAINER_VARIANTS = {
+    visible: {
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.05
+      }
+    }
+  };
+
+  const LIST_ITEM_VARIANTS = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
   return (
 
     <>
       <div className="max-w-2xl m-auto p-8 font-[family-name:var(--font-geist-sans)] flex flex-col h-screen gap-4 cursor-default">
-        <main className="grow">
-          <h1 className="my-2 text-lg hover:text-green-600 dark:hover:text-green-400 transition-colors duration-400">@fsd-niraj</h1>
-          <p className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">{homePageData.summary}
-          </p>
-          <p className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">
+        <motion.main className="grow" variants={LIST_CONTAINER_VARIANTS} initial="hidden" animate="visible">
+          <motion.h1 variants={LIST_ITEM_VARIANTS} className="my-2 text-lg hover:text-green-600 dark:hover:text-green-400 transition-colors duration-400">@fsd-niraj</motion.h1>
+          <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">{homePageData.summary}
+          </motion.p>
+          <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">
             Much interested in <FlipWords words={homePageData.interests} duration={1500} />
-          </p>
-          <p>Cracks that inspire me daily: {homePageData.following.map((name, i) =>
+          </motion.p>
+          <motion.p variants={LIST_ITEM_VARIANTS}>Cracks that inspire me daily: {homePageData.following.map((name, i) =>
             <Anchor href={name.href} title={name.name} key={i} />
           )}
-          </p>
-          <h2 className="my-3">What did I do?</h2>
-          <p className="text-gray-500 dark:text-gray-500">
+          </motion.p>
+          <motion.h2 variants={LIST_ITEM_VARIANTS} className="my-3">What did I do?</motion.h2>
+          <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-gray-500">
             - worked at an Ed-tech startup for more than <Highlight text="2" /> years in Mumbai, India. <br />
             - freelanced for <Highlight text="3+" /> companies and did <Highlight text="one" /> contract for <Highlight text="two" /> months. <br />
             - did a <Highlight text="LOTTT" /> of open-source contributions. And still doing.
-          </p>
-          <h2 className="my-3">Projects – trying to solve real ife problems.</h2>
-          <p className="text-gray-500 dark:text-gray-500">
+          </motion.p>
+          <motion.h2 variants={LIST_ITEM_VARIANTS} className="my-3">Projects – trying to solve real ife problems.</motion.h2>
+          <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-gray-500">
             - AI based brain tumor detector. <br />
             - CV based real time helmet detector. <br />
             - Resume builder which allowed me to apply 100+ jobs with each tailored resume.
-          </p>
-          <p className="my-4">Open for project discussions, coffee-chats <Anchor href="mailto:develop.niraj@gmail.com" title="develop.niraj@gmail.com" /> </p>
-        </main>
+          </motion.p>
+          <motion.p variants={LIST_ITEM_VARIANTS} className="my-4">Open for project discussions, coffee-chats <Anchor href="mailto:develop.niraj@gmail.com" title="develop.niraj@gmail.com" /> </motion.p>
+        </motion.main>
         <footer>
-          <div className="flex justify-center tracking-tight gap-4">
+          <motion.div variants={LIST_CONTAINER_VARIANTS}
+            initial="hidden" animate="visible" className="flex justify-center tracking-tight gap-4">
             {homePageData.social.map((s, i) =>
               <Anchor href={s.href} title={s.name} key={i} />
             )}
-          </div>
+          </motion.div>
         </footer>
       </div>
     </>
