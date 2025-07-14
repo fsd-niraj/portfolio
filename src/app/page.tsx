4282e2f0fe93d6e628e85eac8b256caac02b14ef
fsd-niraj <develop.niraj@gmail.com>
2025-07-14 19:52:17 -0400

    updarte home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <div className="max-w-2xl m-auto p-8 font-[family-name:var(--font-geist-sans)] flex flex-col h-screen gap-4 cursor-default">
         <motion.main className="grow" variants={LIST_CONTAINER_VARIANTS} initial="hidden" animate="visible">
           <motion.h1 variants={LIST_ITEM_VARIANTS} className="my-2 hover:text-green-600 dark:hover:text-green-400 transition-colors duration-400">@fsd-niraj</motion.h1>
-          <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">{homePageData.summary}
+          <motion.p variants={LIST_ITEM_VARIANTS} className="leading-snug mt-4 mb-6">{homePageData.summary}
           </motion.p>
           <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-zinc-400 leading-snug mt-4 mb-6">Trying to be a <span className="line-through">100x dev</span> 1x better dev daily.
           </motion.p>
@@ -54,8 +54,11 @@ export default function Home() {
             <Anchor href={name.href} title={name.name} key={i} />
           )}
           </motion.p>
-          <motion.h2 variants={LIST_ITEM_VARIANTS} className="my-3">What did I do?</motion.h2>
+          <motion.h2 variants={LIST_ITEM_VARIANTS} className="my-3">What I do?</motion.h2>
           <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-gray-500">
+            <motion.h4>Present</motion.h4>
+            - currently supervising as an Venue IT Engineer with Legends. <br />
+            <motion.h4>Past</motion.h4>
             - worked at an Ed-tech startup for more than <Highlight text="2" /> years in Mumbai, India. <br />
             - freelanced for <Highlight text="3+" /> companies and did <Highlight text="one" /> contract for <Highlight text="two" /> months. <br />
             - did a <Highlight text="LOTTT" /> of open-source contributions. And still doing.
@@ -64,7 +67,8 @@ export default function Home() {
           <motion.p variants={LIST_ITEM_VARIANTS} className="text-gray-500 dark:text-gray-500">
             - AI based brain tumor detector. <br />
             - CV based real time helmet detector. <br />
-            - Resume builder which allowed me to apply 100+ jobs with each tailored resume.
+            - Resume builder which allowed me to apply 100+ jobs with each tailored resume. <br />
+            - Personal expense tracker app (in progress)
           </motion.p>
           <motion.p variants={LIST_ITEM_VARIANTS} className="my-4">Open for project discussions, coffee-chats <Anchor href="mailto:develop.niraj@gmail.com" title="develop.niraj@gmail.com" /> </motion.p>
         </motion.main>
