4bb7fc19a3800c1a7156a75edac2ddc0a924a19f
fsd-niraj <develop.niraj@gmail.com>
2026-03-19 01:44:03 +0530

    Adjust NODE_ENV as per Next.js and env standards

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -3,10 +3,10 @@ import { readFile } from "fs/promises";
 import path from "path";
 import { decompress } from "wawoff2";
 import { siteConfig } from "@/data/site-config";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-const env = process.env.NODE_ENV || 'development'
-dotenv.config({ path: `.env.${env}` })
+const env = process.env.NODE_ENV || "development";
+dotenv.config({ path: `.env.${env}` });
 
 export const alt = siteConfig.title;
 
